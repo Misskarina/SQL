9546879675b6d050375fc615005835ca10352197
Misskarina <karinakumari907ce@gmail.com>
2025-09-29 16:30:04 +0530

    How to push any project

---
+++ GitCommands.sql
@@ -0,0 +1,28 @@
+
+
+C:\Users\MISS KARINA>cd C:\Users\MISS KARINA\Desktop\GitSql
+
+C:\Users\MISS KARINA\Desktop\GitSql>git init
+
+C:\Users\MISS KARINA\Desktop\GitSql>git branch
+
+
+C:\Users\MISS KARINA\Desktop\GitSql>git remote set-url origin https://github.com/Misskarina/SQL.git
+
+C:\Users\MISS KARINA\Desktop\GitSql>git remote -v
+
+
+C:\Users\MISS KARINA\Desktop\GitSql>git add .
+
+C:\Users\MISS KARINA\Desktop\GitSql>git commit -m " Insert Data into table"
+
+
+C:\Users\MISS KARINA\Desktop\GitSql>git pull origin master
+
+
+C:\Users\MISS KARINA\Desktop\GitSql>git add .
+
+C:\Users\MISS KARINA\Desktop\GitSql>git rebase
+Current branch master is up to date.
+
+C:\Users\MISS KARINA\Desktop\GitSql>git push origin master
